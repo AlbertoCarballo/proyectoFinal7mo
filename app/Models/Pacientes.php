1b1667bd9cfdef77a4cae833b8ffcae8e235f71d
AlbertoCarballo <jesusalbec_21@alu.uabcs.mx>
2024-12-01 01:28:02 -0700

    resolucion de agregar paciente

--- app/Models/Pacientes.php
+++ app/Models/Pacientes.php
@@ -7,22 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Pacientes extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $table = 'pacientes';
 
     protected $fillable=[
-        "nombre",
-        "primer_apellido",
-        "sefundo_apellido",
-        "fecha_nac",
-        "correo_electronico",
-        "contrasena",
-        "genero",
-        "peso",
-        "estatura",
-        "tipo_sangre",
-        "numero_telefono",
-        "numero_emergencia",
-        "nss",
+        'nombre', 'primer_apellido', 'segundo_apellido', 'fecha_nac',
+        'correo_electronico', 'contrasena', 'genero', 'peso', 'estatura',
+        'tipo_sangre', 'numero_telefonico', 'numero_emergencia', 'nss'
 
     ];
 
