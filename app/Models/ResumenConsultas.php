4dd11886416cae4d015cc5aa96e778c0454410c6
AlbertoCarballo <jesusalbec_21@alu.uabcs.mx>
2024-12-03 20:46:30 -0700

    creacion del CRUD de las citas, falta redireccionar

--- app/Models/ResumenConsultas.php
+++ app/Models/ResumenConsultas.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Doctores;
 
 class ResumenConsultas extends Model
 {
-    //
+    use HasFactory;
+    public $timestamps = false;
+    protected $primaryKey = 'id_resumen_consulta';
+    protected $table = 'resumenConsulta';
+    protected $fillable = ['id_consulta_medica', 'nombre_paciente', 'id_doctor',
+                            'nombre_doctor', 'fecha_consulta', 'resumen_consulta'];
+    public function foraneas()
+    {
+        return $this->belongsTo(Doctores::class, 'id_doctor');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,27 +6,36 @@ use App\Http\Controllers\Api\PacientesController;
 use App\Http\Controllers\Api\DoctoresController;
 use App\Http\Controllers\Api\EspecialidadesController;
 use App\Http\Controllers\Api\authController;
+use App\Http\Controllers\Api\citasController;
 
 //Ruta Auth
 Route::post("/auth",[authController::class, 'login']);
 
 //Rutas para Pacientes
 
-Route::get('/pacientes', [PacientesController::class, 'getPacientes']);
-Route::get('/pacientes/{id}', [PacientesController::class, 'getSpecificPacientes']);
-Route::post("/pacientes",[PacientesController::class, 'postPacientes']);
-Route::put("/pacientes/{id}",[PacientesController::class, 'updatePacientes']);
-Route::delete("/pacientes/{id}",[PacientesController::class, 'deletePaciente']);
+Route::get('/ver-pacientes', [PacientesController::class, 'getPacientes']);
+Route::get('/ver-un-paciente/{id}', [PacientesController::class, 'getSpecificPacientes']);
+Route::post("/crear-pacientes",[PacientesController::class, 'postPacientes']);
+Route::put("/actualizar-pacientes/{id}",[PacientesController::class, 'updatePacientes']);
+Route::delete("/borrar-pacientes/{id}",[PacientesController::class, 'deletePaciente']);
 
 
 //Rutas para Doctores
-Route::get('doctores/{correo}', [DoctoresController::class, 'getDoctorByCorreo']);
-Route::get('/doctores', [DoctoresController::class, 'getDoctores']);
-Route::get('/doctores/{id}', [DoctoresController::class, 'getSpecificDoctor']);
-Route::post("/doctores",[DoctoresController::class, 'postDoctor']);
-Route::put("/doctores/{id}",[DoctoresController::class, 'updateDoctor']);
-Route::delete("/doctores/{id}",[DoctoresController::class, 'deleteDoctor']);
+Route::get('/ver-mi-doctor/{correo}', [DoctoresController::class, 'getDoctorByCorreo']);
+Route::get('/ver-doctores', [DoctoresController::class, 'getDoctores']);
+Route::get('/ver-un-doctor/{id}', [DoctoresController::class, 'getSpecificDoctor']);
+Route::post("/crear-doctores",[DoctoresController::class, 'postDoctor']);
+Route::put("/actualizar-doctor/{id}",[DoctoresController::class, 'updateDoctor']);
+Route::delete("/borrar-doctores/{id}",[DoctoresController::class, 'deleteDoctor']);
 
 
 //Ruta especialidad
 Route::get('/especialidades', [EspecialidadesController::class, 'getEspecialidades']);
+
+//Rutas para citas
+Route::get('/ver-citas', [citasController::class, 'getCitas']);
+Route::get('/ver-mis-citas/{id}', [citasController::class, 'getMisCitas']);
+Route::get('/ver-una-cita/{id}', [citasController::class, 'getSpecificCita']);
+Route::post("/crear-cita",[citasController::class, 'postCitas']);
+Route::put("/actualizar-cita/{id}",[citasController::class, 'updateCita']);
+Route::delete("/borrar-cita/{id}",[citasController::class, 'deleteCita']);
